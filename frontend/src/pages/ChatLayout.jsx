d5088702b292406553cf6add4dac458793cb9d31
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2026-01-11 10:47:55 +0530

    feat: add ChatLayout component

--- frontend/src/pages/ChatLayout.jsx
+++ frontend/src/pages/ChatLayout.jsx
@@ -1,5 +1,20 @@
-export default function ChatLayout() {
-	return <>
-		<p>laoutr</p>
-	</>
-}
+import { Outlet } from "react-router-dom";
+import InboxSidebar from "../components/InboxSidebar";
+
+const ChatLayout = () => {
+	return (
+		<div className="flex h-screen w-screen overflow-hidden">
+			{/* LEFT: Inbox (persistent) */}
+			<div className="w-[360px] border-r border-gray-700">
+				<InboxSidebar />
+			</div>
+
+			{/* RIGHT: Chat area (route-driven) */}
+			<div className="flex-1 bg-gray-900">
+				<Outlet />
+			</div>
+		</div>
+	);
+};
+
+export default ChatLayout;
